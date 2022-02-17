bacd33265173c84855d894cd7c4bb725bcf5b878
AA137 <35202680+AA137@users.noreply.github.com>
2022-02-16 21:35:21 -0500

    Create pathSim.py

---
+++ pathSim.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def pathSimulator(spot, strike, dte, iv, rho, barrier, iters, dt = 0.05, american = True, long = True):
+    return None
